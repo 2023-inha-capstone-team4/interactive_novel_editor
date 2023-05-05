2324bcf0abccc483942829171033084214a12c5c
kimjung6408 <kimjung6408@gmail.com>
2023-05-05 16:03:41 +0900

    feat : json을 sceneList로 전환하는 코드 추가

--- src/lib/dataParser/JsonParser.js
+++ src/lib/dataParser/JsonParser.js
@@ -7,13 +7,6 @@ import {Scene} from "../Scene";
  * 프로젝트를 json으로 변환하거나,
  * json을 프로젝트 파일로 변환합니다. * 
  * 
- * @function changeSceneListToJson
- * @param sceneList
- * 
- * 
- * 
- * @function changeJsonToSceneList
- *  @param jsonObjectr
  * 
  * 
  */
@@ -84,5 +77,17 @@ export class JsonParser
         }
       
         return scene;
+      }
+
+      static jsonToSceneList(json) {
+        const sceneListJson = JSON.parse(json);
+        const sceneList = [];
+      
+        for (const sceneJson of sceneListJson) {
+          const scene = JsonParser.jsonToScene(sceneJson);
+          sceneList.push(scene);
+        }
+      
+        return sceneList;
       }
 }
