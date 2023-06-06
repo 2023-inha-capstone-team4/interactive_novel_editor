efa2b6d4581672033fd3b17910957e357d4d8e69
Woody <gintooooonic@gmail.com>
2023-06-06 16:32:02 +0900

    작가 서비스 연동

--- src/index.js
+++ src/index.js
@@ -19,3 +19,7 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// 작가 서비스에서 사용
+const EditorApp = App;
+export { EditorApp }
